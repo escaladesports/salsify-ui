7882cbfc6ee05d6f148ebc975f82db8f8160d2c2
Taylor <tbaustin1992@gmail.com>
2018-10-08 13:37:44 -0500

    merge

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,16 +1,16 @@
 // import fetch from "isomorphic-fetch"
 import axios from "axios"
 
-const deleteProperties = async properties => {
+const deleteProperties = async () => {
 	await axios({
 		url: `https://app.salsify.com/api/v1/orgs/${
 			process.env.GATSBY_SALSIFY_ORG
-		}/properties`,
+		}/properties/123abc`,
 		method: `delete`,
 		headers: {
 			Authorization: `Bearer ${process.env.GATSBY_SALSIFY_API_TOKEN}`,
 		},
-		data: { ids: properties },
+		// data: { ids: properties },
 	})
 }
 
